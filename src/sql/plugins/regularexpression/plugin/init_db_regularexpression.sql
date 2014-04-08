99e08b486366380f20040a25e14bb13b109c18d4
RPU <RPU@bab10101-e421-0410-a517-8ce0973de3ef>
2014-04-08 08:26:56 +0000

    REGEXP-9 Missing data
    git-svn-id: http://dev.lutece.paris.fr/svn/lutece/portal/trunk/plugins/technical/plugin-regularexpression@65568 bab10101-e421-0410-a517-8ce0973de3ef

--- src/sql/plugins/regularexpression/plugin/init_db_regularexpression.sql
+++ src/sql/plugins/regularexpression/plugin/init_db_regularexpression.sql
@@ -2,3 +2,4 @@
 -- Dumping data in table regularexpression_regular_expression
 --
 INSERT INTO regularexpression_regular_expression (id_expression, title, regular_expression_value, valid_exemple, information_message, error_message) VALUES ('1', 'Fichier JPG', 'image\/jpeg', 'image/jpeg', 'Expression régulière pour les fichiers de type jpeg.', 'Le format du fichier n''est pas valide. Veuillez choisir une image de type jpeg.');
+INSERT INTO regularexpression_regular_expression (id_expression, title, regular_expression_value, valid_exemple, information_message, error_message) VALUES ('2', 'Email', '(^([a-zA-Z0-9]+(([\\.\\-\\_]?[a-zA-Z0-9]+)+)?)\\@(([a-zA-Z0-9]+[\\.\\-\\_])+[a-zA-Z]{2,4})$)|(^$)', 'admin@lutece.fr', 'Expression régulière pour les emails.', 'Le format de l''email est incorrect.');
---
+++ src/sql/plugins/regularexpression/upgrade/update_db_regularexpression_3.0.0_3.0.1.sql
@@ -0,0 +1,1 @@
+INSERT INTO regularexpression_regular_expression (id_expression, title, regular_expression_value, valid_exemple, information_message, error_message) VALUES ('2', 'Email', '(^([a-zA-Z0-9]+(([\\.\\-\\_]?[a-zA-Z0-9]+)+)?)\\@(([a-zA-Z0-9]+[\\.\\-\\_])+[a-zA-Z]{2,4})$)|(^$)', 'admin@lutece.fr', 'Expression régulière pour les emails.', 'Le format de l''email est incorrect.');
